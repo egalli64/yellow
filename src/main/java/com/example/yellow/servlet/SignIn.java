769338158e891ea35d31cb96441250daa42d63ac
Lorena Carbone <lorenacarbone4@icloud.com>
2022-06-01 14:39:59 +0200

    newcommit

--- src/main/java/com/example/yellow/servlet/SignIn.java
+++ src/main/java/com/example/yellow/servlet/SignIn.java
@@ -1,12 +1,13 @@
 package com.example.yellow.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 
 /**
  * Servlet implementation class SignIn
@@ -15,25 +16,29 @@ import javax.servlet.http.HttpServletResponse;
 public class SignIn extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException {
-			String firstname = request.getParameter("firstname");
-			String lastname = request.getParameter("lastname");
-			String email = request.getParameter("email");
-			String password = request.getParameter("password");
-			String username = request.getParameter("username");
-			
-			String url;
-			
-			if (firstname == null || firstname.isBlank()|| lastname == null || lastname.isBlank() || email == null || email.isBlank() || password == null || password.isBlank() || username == null || username.isBlank()) {
-				url="index.jsp";			
+			throws ServletException, IOException {
+		String firstname = request.getParameter("firstname");
+		String lastname = request.getParameter("lastname");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		String username = request.getParameter("username");
+
+		String url;
+
+		if (firstname == null || firstname.isBlank() || lastname == null || lastname.isBlank() || email == null
+				|| email.isBlank() || password == null || password.isBlank() || username == null
+				|| username.isBlank()) {
+			url = "index.jsp";
+		} else {
+			url = "home.jsp";
+		}
+		RequestDispatcher rd = request.getRequestDispatcher(url);
+		rd.forward(request, response);
 	}
-			else {
-				url= "home.html";
-			}
-	}
+
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,3 +1,6 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+	pageEncoding="utf-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -24,14 +27,14 @@
             <div class="form-floating">
                 <input name="lastname" class="form-control" id="floatingCognome" placeholder="Rossi" required>
                 </div>
-        <div class ="form-floating">
-        <input type ="email" class="form-control" id="floatingInput" placeholder="name@example.com" required>
+        <div class="form-floating">
+        <input name="email" class="form-control" id="floatingInput" placeholder="name@example.com" required>
         </div>
-        <div class ="form-floating">
-        <input type ="password" class="form-control" id="floatingPassword" placeholder="password" required>
+        <div class="form-floating">
+        <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="password" required>
         </div>
-        <div class ="form-floating">
-            <input name ="username" class="form-control" id="floatingUsername" placeholder="Username" required>
+        <div class="form-floating">
+            <input name="username" class="form-control" id="floatingUsername" placeholder="Username" required>
             </div>
           <div class="checkbox mb-3">
             <label><input type="checkbox" value="remember-me">Remember me</label>
