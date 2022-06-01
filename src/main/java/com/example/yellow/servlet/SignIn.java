570d1dd2177bd6e41457729cdfef5a5f729562ea
Lorena Carbone <lorenacarbone4@icloud.com>
2022-06-01 11:02:08 +0200

    newjob

--- src/main/java/com/example/yellow/servlet/SignIn.java
+++ src/main/java/com/example/yellow/servlet/SignIn.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 /**
  * Servlet implementation class SignIn
  */
@@ -14,11 +15,33 @@ import javax.servlet.http.HttpServletResponse;
 public class SignIn extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("***********");
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+			String firstname = request.getParameter("firstname");
+			if (firstname == null || firstname.isBlank()) {
+				firstname = "Unknow";
+			}
+			String lastname = request.getParameter("lastname");
+			if (lastname == null || lastname.isBlank()) {
+				lastname = "Unknow";
+			}
+			String email = request.getParameter("email");
+			if (email == null || email.isBlank()) {
+				email = "Unknow";
+			}
+			String password = request.getParameter("password");
+			if (password == null || password.isBlank()) {
+				password = "Unknow";
+			}
+			String username = request.getParameter("username");
+			if (username == null || username.isBlank()) {
+				username = "Unknow";
+			}
+			
 	}
 
 }
