b157ee3f138032e99f9ffe2c6683eb328fdaf375
seren <seren@DESKTOP-F472530.homenet.telecomitalia.it>
2022-06-06 15:05:07 +0200

    changes in Login.java

--- src/main/java/com/example/yellow/dao/UserDao.java
+++ src/main/java/com/example/yellow/dao/UserDao.java
@@ -2,11 +2,13 @@ package com.example.yellow.dao;
 
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.sql.DataSource;
 
@@ -16,6 +18,12 @@ import org.apache.logging.log4j.Logger;
 public class UserDao implements AutoCloseable {
 	private static final Logger log = LogManager.getLogger(UserDao.class);
 	private static final String GET_ALL = "SELECT user_id, first_name, last_name, email, password, username FROM user";
+	private static final String INSERT = "insert into user(first_name, last_name, email, password, username) values (?, ?, ?, ?, ?)";
+	private static final String GET_BY_USER = """
+			SELECT user_id, first_name, last_name, email, password, username
+			FROM user
+			where username = ? AND password = ?
+			""";
 	private Connection conn;
 
 	public UserDao(DataSource ds) {
@@ -35,7 +43,8 @@ public class UserDao implements AutoCloseable {
 		try (Statement stmt = conn.createStatement(); //
 				ResultSet rs = stmt.executeQuery(GET_ALL)) {
 			while (rs.next()) {
-				User cur = new User(rs.getInt(1), rs.getString(2), rs.getString(3), null, null, null);
+				User cur = new User(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5),
+						rs.getString(6));
 				results.add(cur);
 			}
 		} catch (SQLException se) {
@@ -44,6 +53,42 @@ public class UserDao implements AutoCloseable {
 		}
 
 		return results;
+	}
+
+	public boolean insert(User user) {
+		try (PreparedStatement ps = conn.prepareStatement(INSERT)) {
+			ps.setString(1, user.getFirstName());
+			ps.setString(2, user.getLastName());
+			ps.setString(3, user.getEmail());
+			ps.setString(4, user.getPassword());
+			ps.setString(5, user.getUsername());
+			if (ps.executeUpdate() == 1) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (SQLException se) {
+			log.error("Can't save coder " + user.getId(), se);
+			return false;
+		}
+	}
+
+	public Optional<User> get(String username, String password) {
+		log.trace("called");
+
+		try (Statement stmt = conn.createStatement(); //
+				ResultSet rs = stmt.executeQuery(GET_BY_USER)) {
+			if (rs.next()) {
+				User cur = new User(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5),
+						rs.getString(6));
+				return Optional.of(cur);
+			} else {
+				return Optional.empty();
+			}
+		} catch (SQLException se) {
+			log.error("Can't get users: " + se.getMessage());
+			throw new IllegalStateException("Database issue " + se.getMessage());
+		}
 	}
 
 	@Override
